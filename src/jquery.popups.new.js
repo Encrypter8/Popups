1f0fc0140dc0e522fad3c3d31eb5a0218fb1481f
Harris Miller <harrismiller08@gmail.com>
2013-10-31 16:16:27 -0600

    Added support for window resize

--- src/jquery.popups.new.js
+++ src/jquery.popups.new.js
@@ -85,6 +85,13 @@
 
 		//TODO
 		// set triggering element with event to open/close dialog
+
+		// auto re-position on window resize
+		this.windowEvent = $window.on('resize.popup', function() {
+			if (that.isOpen) {
+				that.positionPopup();
+			}
+		});
 	};
 
 	Popup.prototype.positionPopup = function() {
@@ -242,6 +249,7 @@
 			this.$attachTo.removeData('popup-ref');
 		}
 		this.$el.trigger('destroy.popup');
+		$window.off(this.windowEvent);
 		this.$popup.remove();
 	};
 
