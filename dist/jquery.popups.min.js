7d84f3de731ae80edd680d7d2836508dc22501bd
unknown <harris.miller@BOUPMAPC058.markit.partners>
2014-05-13 14:41:38 -0600

    updated version to 1.0.0-RC1

--- dist/jquery.popups.min.js
+++ dist/jquery.popups.min.js
@@ -1,4 +1,4 @@
-/* jQuery Popups - v1.0.0-beta - 2014-05-13
+/* jQuery Popups - v1.0.0-RC1 - 2014-05-13
  * https://github.com/harris-miller/Popups
  * Copyright (c) 2014 Harris Miller
  * Licensed MIT 
