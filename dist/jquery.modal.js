c8ef5f39b084ce11e0628fecf1f34179d9def353
Harris Miller <harrismiller08@gmail.com>
2014-08-18 20:45:20 -0600

    brought over dist folder, fixed index.html

---
+++ dist/jquery.modal.js
@@ -0,0 +1,40 @@
+/* jQuery Modal - v1.0.0 - 2014-06-02
+ * https://github.com/harris-miller/Popups
+ * Copyright (c) 2014 Harris Miller
+ * Licensed MIT 
+ */
+
+
++function ($, document, window) {
+
+	"use strict";
+
+	var cachedScrollbarWidth;
+
+
+	// credit: jQuery UI
+	function scrollbarWidth() {
+		if (cachedScrollbarWidth !== undefined) {
+			return cachedScrollbarWidth;
+		}
+		var div = $('<div style="display:block;position:absolute;width:50px;height:50px;overflow:hidden;""><div style="height:100px;width:auto;""></div></div>'),
+			innerDiv = div.children()[0],
+			w1, w2;
+
+		$('body').append(div);
+		w1 = innerDiv.offsetWidth;
+		div.css('overflow', 'scroll');
+
+		w2 = innerDiv.offsetWidth;
+
+		if (w1 === w2) {
+			w2 = div[0].clientWidth;
+		}
+
+		div.remove();
+
+		cachedScrollbarWidth = w1 - w2;
+		return cachedScrollbarWidth;
+	}
+
+ }(jQuery, document, window);
